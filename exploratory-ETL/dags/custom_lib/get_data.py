4314d18309b1bdce718300881740a769b527c1d5
MJMukwevho <mjmukwevho@outlook.com>
2024-06-02 16:14:34 +0200

    solve get data issues

--- exploratory-ETL/dags/custom_lib/get_data.py
+++ exploratory-ETL/dags/custom_lib/get_data.py
@@ -3,8 +3,8 @@ This module uses the request the request module to access
 data in a random user api and mimick a login session
 """
 
-import requests
 import json
+import requests
 
 
 def get_data():
@@ -14,9 +14,13 @@ def get_data():
     try:
         url = "https://randomuser.me/api/"
         response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
         status_code = response.status_code
-        response = json.loads(response.text)
-        return status_code, response["results"][0]
-    except Exception as e:
-        print(f"there was an error{e}")
+        response_json = response.json()
+        return status_code, response_json["results"][0]
+    except requests.exceptions.RequestException as e:
+        print(f"There was a network error: {e}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"There was an error decoding the JSON response: {e}")
         return None
