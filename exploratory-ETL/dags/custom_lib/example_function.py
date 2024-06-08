da785f83b1179c7fbee73055a6feb586f0d6dca9
MJMukwevho <mjmukwevho@outlook.com>
2024-06-08 20:30:31 +0200

    add env varibale

--- exploratory-ETL/dags/custom_lib/example_function.py
+++ exploratory-ETL/dags/custom_lib/example_function.py
@@ -1,4 +1,12 @@
-def example_function():
+"""
+All good code deserve a docstring
+"""
+
+
+def example_function(df):
+    """
+    Example docstring
+    """
     print("This is a print statement")
     df.show()
 
